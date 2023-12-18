c2ee54252b011eb8f77e1cacbfe7aaf295083b83
automata-killa <53969619+Automata02@users.noreply.github.com>
2023-12-18 15:38:21 +0200

    props

--- App/constants/colors.js
+++ App/constants/colors.js
@@ -1,5 +1,7 @@
 export default {
     text: "#343434",
     borderColor: "#E2E2E2",
-    blue: "#4f6d7a"
+    blue: "#4f6d7a",
+    white: "#ffffff",
+    textLight: "#797979"
 }
--- App/screens/Home.js
+++ App/screens/Home.js
@@ -1,11 +1,28 @@
 import React from "react";
-import { View, StyleSheet, StatusBar } from "react-native";
+import { View, StyleSheet, StatusBar, Image, Dimensions } from "react-native";
 import colors from '../constants/colors';
+import { ConversionInput } from "../components/ConversionInput";
+
+const screen = Dimensions.get('window');
 
 const styles = StyleSheet.create({
     container: {
         backgroundColor: colors.blue,
-        flex: 1
+        flex: 1,
+        justifyContent: "center"
+    },
+    logoContainer: {
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    logoBackground: {
+        width: screen.width * 0.45,
+        height: screen.width * 0.45
+    },
+    logo: {
+        position: "absolute",
+        width: screen.width * 0.25,
+        height: screen.width * 0.25
     }
 });
 
@@ -13,6 +30,24 @@ export default () => {
     return (
         <View style={styles.container}>
             <StatusBar barStyle="light-content" />
+            <View style={styles.logoContainer}>
+                <Image source={require('../assets/images/background.png')} style={styles.logoBackground} resizeMode="contain"/>
+                <Image source={require('../assets/images/logo.png')} style={styles.logo} resizeMode="contain"/>
+            </View>
+
+            <ConversionInput
+                text="USD"
+                value="123"
+                onButtonPress={() => alert('TODO!')}
+                onChangeText={text => console.log("text", text)}
+                keyboardType="numeric"
+            />
+
+            <ConversionInput
+                text="GBP"
+                value="123"
+                onButtonPress={() => alert('TODO!')}
+            />
         </View>
     )
 };
