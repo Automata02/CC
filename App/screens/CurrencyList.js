f9b3b01f85ea8fbe1809a5ed3118f12bc10b57c6
automata-killa <53969619+Automata02@users.noreply.github.com>
2023-12-19 12:44:11 +0200

    Selected Currency

--- App/screens/CurrencyList.js
+++ App/screens/CurrencyList.js
@@ -1,13 +1,26 @@
 import React from "react"
-import { StatusBar, FlatList, View } from "react-native";
+import { StatusBar, FlatList, View, StyleSheet } from "react-native";
 import { useSafeArea } from 'react-native-safe-area-context';
+import { Entypo } from '@expo/vector-icons'
 
 import colors from "../constants/colors"
 import currencies from '../data/currencies.json'
 import { RowItem, RowSeparator } from '../components/RowItems'
 
-export default ({ navigation }) => {
+const styles = StyleSheet.create({
+    icon: {
+      width: 30,
+      height: 30,
+      borderRadius: 20,
+      alignItems: "center",
+      justifyContent: "center",
+      backgroundColor: colors.blue,
+    },
+  })
+
+export default ({ navigation, route = {} }) => {
     const insets = useSafeArea();
+    const params = route.params || {};
 
     return (
         <View style={{ backgroundColor: colors.white }}>
@@ -15,10 +28,18 @@ export default ({ navigation }) => {
             <FlatList
                 data={currencies}
                 renderItem={({ item }) => {
+                    const selected = params.activeCurrency === item;
                     return (
                         <RowItem
                             text={item}
                             onPress={() => navigation.pop()}
+                            rightIcon={
+                                selected && (
+                                    <View style={styles.icon}>
+                                        <Entypo name="check" size={20} color={colors.white}/>
+                                    </View>
+                                )
+                            }
                         />
                     )
                 }}
